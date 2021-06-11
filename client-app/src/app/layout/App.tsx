05c8cd2b7404d138107b805501591fb8e47061ba
Chad Schadewald <ces83@students.uwf.edu>
2021-06-11 17:43:06 -0500

    48 - NavBar and styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       setActivities(response.data);
     })
   }, [])
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+      <NavBar />
 
         <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
